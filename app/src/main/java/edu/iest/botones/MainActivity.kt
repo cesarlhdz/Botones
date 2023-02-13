670d15db626b21b5db6cfc994f5b9c7602466a47
Cesar Luna <cesar.luna@iest.edu.mx>
2023-02-13 17:51:08 -0600

    270123

--- app/src/main/java/edu/iest/botones/MainActivity.kt
+++ app/src/main/java/edu/iest/botones/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.iest.botones
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,17 +9,30 @@ import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvFrase : TextView
+    private lateinit var etFrase:  EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tvFrase : TextView
         tvFrase = findViewById(R.id.textView)
-        var etFrase:  EditText
         etFrase = findViewById(R.id.editTextFrase)
         var bnMostrar = findViewById<Button>(R.id.buttonMostrar)
         bnMostrar.setOnClickListener{
-            val frase= etFrase.text.toString()
+            val frase= obtenerFrase()
             Snackbar.make(tvFrase, "Su frase fue $frase", Snackbar.LENGTH_LONG).show()
         }
+
+        var bnSiguiente= findViewById<Button>(R.id.bnSiguiente)
+        bnSiguiente.setOnClickListener{
+            val i = Intent(this, SegundaActivity::class.java)
+            i.putExtra("frase", etFrase.text.toString()) //enviamos
+            i.putExtra("edad", 21) //enviamos dato "edad" con valor 21
+            startActivity(i)
+        }
+        }
+         private fun obtenerFrase() : String{
+             return etFrase.text.toString()
     }
 }
--- app/src/main/java/edu/iest/botones/SegundaActivity.kt
+++ app/src/main/java/edu/iest/botones/SegundaActivity.kt
@@ -3,18 +3,46 @@ package edu.iest.botones
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class SegundaActivity : AppCompatActivity() {
+
+    private var cambioIcono : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        var fraseRecibida = intent.getStringExtra("frase")
+        var edad = intent.getIntExtra("edad", 0)
+
+        Log.d("DATOS", "Datos recibidos con exito fueron frase $fraseRecibida y edad $edad" )
+
         var bnAbrirPantalla= findViewById<Button>(R.id.bnPantalla)
+        var ivLogo = findViewById<ImageView>(R.id.ivLogo)
+        ivLogo.setOnClickListener{
+            var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest)
+            if (cambioIcono) {
+                ivLogo.setImageResource(arregloImagenes[0])
+            }else {
+                ivLogo.setImageResource(arregloImagenes[1])
+            }
+            cambioIcono = !cambioIcono
+            //ivLogo.setImageResource(R.drawable.anahuac)
+        }
+
         bnAbrirPantalla.setOnClickListener{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
+            finish()
         }
 
         var bnCerrar= findViewById<Button>(R.id.bnCerrar)
+        bnCerrar.setOnClickListener {
+            finish()
+        }
     }
 }
